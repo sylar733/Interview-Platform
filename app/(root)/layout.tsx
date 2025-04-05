6244f3e15165e55e6f88bee3ff3f7a88a52a9a36
Angelo <luis.arzanan992@gmail.com>
2025-04-05 14:37:18 +0800

    fixed redirect

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,48 +1,26 @@
-/* eslint-disable @next/next/no-html-link-for-pages */
-'use client';
-
-import { useEffect, useState } from 'react';
-import { useRouter, usePathname } from 'next/navigation';
-import { isAuthenticated } from '@/lib/actions/auth.action';
+import Link from 'next/link';
 import Image from 'next/image';
 import { ReactNode } from 'react';
+import { redirect } from 'next/navigation';
 
-const RootLayout = ({ children }: { children: ReactNode }) => {
-  const [isUserAuthenticated, setIsUserAuthenticated] = useState<boolean | null>(null);
-  const router = useRouter();
-  const pathname = usePathname(); // ✅ Make sure this is inside the component
+import { isAuthenticated } from '@/lib/actions/auth.action';
 
-  useEffect(() => {
-    const checkAuthentication = async () => {
-      const authenticated = await isAuthenticated();
-      console.log('Authenticated:', authenticated);
-
-      setIsUserAuthenticated(authenticated);
-
-      // Only redirect if not on homepage
-      if (!authenticated && pathname !== '/interview') {
-        router.push('/interview');
-      }
-    };
-
-    checkAuthentication();
-  }, [router, pathname]);
-
-  if (isUserAuthenticated === null) {
-    return <div>Loading...</div>;
-  }
+const Layout = async ({ children }: { children: ReactNode }) => {
+  const isUserAuthenticated = await isAuthenticated();
+  if (!isUserAuthenticated) redirect('/sign-in');
 
   return (
     <div className="root-layout">
       <nav>
-        <a href="/" className="flex items-center gap-2">
-          <Image src="/logo.svg" alt="Logo" width={38} height={32} />
+        <Link href="/" className="flex items-center gap-2">
+          <Image src="/logo.svg" alt="MockMate Logo" width={38} height={32} />
           <h2 className="text-primary-100">PrepWise</h2>
-        </a>
+        </Link>
       </nav>
+
       {children}
     </div>
   );
 };
 
-export default RootLayout;
+export default Layout;
