b6da09210d2c94b937523b116a6fc08862428e45
Angelo <luis.arzanan992@gmail.com>
2025-04-05 14:35:04 +0800

    fixed redirect

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,11 +1,12 @@
-import { isAuthenticated } from '@/lib/actions/auth.action';
-import { redirect } from 'next/navigation';
 import { ReactNode } from 'react';
+import { redirect } from 'next/navigation';
+
+import { isAuthenticated } from '@/lib/actions/auth.action';
 
 const AuthLayout = async ({ children }: { children: ReactNode }) => {
   const isUserAuthenticated = await isAuthenticated();
+  if (isUserAuthenticated) redirect('/');
 
-  if (!isUserAuthenticated) redirect('/');
   return <div className="auth-layout">{children}</div>;
 };
 
