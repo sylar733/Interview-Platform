e4d5454d5269cfb3faec83743461dd9bb298e690
Angelo <luis.arzanan992@gmail.com>
2025-04-09 13:17:53 +0800

    small changes of texts

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -93,7 +93,7 @@ const AuthForm = ({ type }: { type: FormType }) => {
           <h2 className="text-primary-100">PrepWise</h2>
         </div>
 
-        <h3>Practice job interview with AI</h3>
+        <h3 className="text-center">Practice job interview with AI</h3>
 
         <Form {...form}>
           <form onSubmit={form.handleSubmit(onSubmit)} className="w-full space-y-6 mt-4 form">
